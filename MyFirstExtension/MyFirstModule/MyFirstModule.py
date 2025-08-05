0a82bb72fb034a403009e2e7ece3711d660af991
Santi <sanolydani@gmail.com>
2025-08-05 17:29:53 +0100

    STYLE Re #3: Update module metadata and documentation.

--- MyFirstExtension/MyFirstModule/MyFirstModule.py
+++ MyFirstExtension/MyFirstModule/MyFirstModule.py
@@ -1,108 +1,80 @@
 import logging
 import os
-from typing import Annotated
-
 import vtk
-
 import slicer
+import SampleData
+from typing import Annotated
 from slicer.i18n import tr as _
 from slicer.i18n import translate
 from slicer.ScriptedLoadableModule import *
 from slicer.util import VTKObservationMixin
+from slicer import vtkMRMLScalarVolumeNode
 from slicer.parameterNodeWrapper import (
     parameterNodeWrapper,
     WithinRange,
 )
 
-from slicer import vtkMRMLScalarVolumeNode
-
-
-#
-# MyFirstModule
-#
-
 
 class MyFirstModule(ScriptedLoadableModule):
-    """Uses ScriptedLoadableModule base class, available at:
-    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+
+    """
+        Uses ScriptedLoadableModule base class, available at:
+        https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
     """
 
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
-        self.parent.title = _("MyFirstModule")  # TODO: make this more human readable by adding spaces
-        # TODO: set categories (folders where the module shows up in the module selector)
+        self.parent.title = _("Sphere-shaped model")
         self.parent.categories = [translate("qSlicerAbstractCoreModule", "Examples")]
-        self.parent.dependencies = []  # TODO: add here list of module names that this module requires
-        self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # TODO: replace with "Firstname Lastname (Organization)"
-        # TODO: update with short description of the module and a link to online module documentation
-        # _() function marks text as translatable to other languages
-        self.parent.helpText = _("""
-This is an example of scripted loadable module bundled in an extension.
-See more information in <a href="https://github.com/organization/projectname#MyFirstModule">module documentation</a>.
-""")
-        # TODO: replace with organization, grant and thanks
-        self.parent.acknowledgementText = _("""
-This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc., Andras Lasso, PerkLab,
-and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
-""")
-
-        # Additional initialization step after application startup is complete
+        self.parent.dependencies = []
+        self.parent.contributors = ["Santiago Ramoli, ULPGC"]
+        self.parent.helpText = _(   
+                                    """
+                                        This is my first scriptable module. 
+                                        The goal is to place a sphere-shaped model with two marking points.
+                                        See more information in <a href="https://github.com/Santiramoli/SlicerTutorial.git"></a>.
+                                    """
+                                )
+        self.parent.acknowledgementText = _("""This file is edited by Santiramoli.""")
         slicer.app.connect("startupCompleted()", registerSampleData)
 
 
-#
-# Register sample data sets in Sample Data module
-#
 
+# Register sample datasets in Sample Data module
 
 def registerSampleData():
+
     """Add data sets to Sample Data module."""
-    # It is always recommended to provide sample data for users to make it easy to try the module,
-    # but if no sample data is available then this method (and associated startupCompeted signal connection) can be removed.
 
-    import SampleData
-
+    # Obtains the path where the thumbnail images are located.
     iconsPath = os.path.join(os.path.dirname(__file__), "Resources/Icons")
 
-    # To ensure that the source code repository remains small (can be downloaded and installed quickly)
-    # it is recommended to store data sets that are larger than a few MB in a Github release.
 
-    # MyFirstModule1
+    # First Dataset
     SampleData.SampleDataLogic.registerCustomSampleDataSource(
-        # Category and sample name displayed in Sample Data module
         category="MyFirstModule",
-        sampleName="MyFirstModule1",
-        # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
-        # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
+        sampleName="Dataset1",
         thumbnailFileName=os.path.join(iconsPath, "MyFirstModule1.png"),
-        # Download URL and target file name
         uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
         fileNames="MyFirstModule1.nrrd",
-        # Checksum to ensure file integrity. Can be computed by this command:
-        #  import hashlib; print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
         checksums="SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
-        # This node name will be used when the data set is loaded
-        nodeNames="MyFirstModule1",
+        nodeNames="FirstNode",
     )
 
-    # MyFirstModule2
+    # Second Dataset
     SampleData.SampleDataLogic.registerCustomSampleDataSource(
-        # Category and sample name displayed in Sample Data module
         category="MyFirstModule",
-        sampleName="MyFirstModule2",
+        sampleName="Dataset2",
         thumbnailFileName=os.path.join(iconsPath, "MyFirstModule2.png"),
-        # Download URL and target file name
         uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
         fileNames="MyFirstModule2.nrrd",
         checksums="SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
-        # This node name will be used when the data set is loaded
-        nodeNames="MyFirstModule2",
+        nodeNames="SecondNode",
     )
 
 
-#
+
 # MyFirstModuleParameterNode
-#
 
 
 @parameterNodeWrapper
