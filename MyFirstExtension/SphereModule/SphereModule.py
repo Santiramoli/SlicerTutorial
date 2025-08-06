d7ec293823617a79be424dc301122afe5a9e6dea
Santi <sanolydani@gmail.com>
2025-08-06 16:04:30 +0100

    STYLE #Re3: simplify SphereModule base structure and remove unused sample data registration

--- MyFirstExtension/SphereModule/SphereModule.py
+++ MyFirstExtension/SphereModule/SphereModule.py
@@ -1,109 +1,37 @@
 import logging
 import os
-from typing import Annotated
-
 import vtk
-
 import slicer
+import SampleData
+import time
+from typing import Annotated
 from slicer.i18n import tr as _
 from slicer.i18n import translate
 from slicer.ScriptedLoadableModule import *
 from slicer.util import VTKObservationMixin
+from slicer import vtkMRMLScalarVolumeNode
 from slicer.parameterNodeWrapper import (
     parameterNodeWrapper,
     WithinRange,
 )
 
-from slicer import vtkMRMLScalarVolumeNode
-
-
-#
-# SphereModule
-#
-
 
 class SphereModule(ScriptedLoadableModule):
-    """Uses ScriptedLoadableModule base class, available at:
-    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
-    """
 
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
-        self.parent.title = _("SphereModule")  # TODO: make this more human readable by adding spaces
-        # TODO: set categories (folders where the module shows up in the module selector)
+        self.parent.title = _("SphereModule")
         self.parent.categories = [translate("qSlicerAbstractCoreModule", "Examples")]
-        self.parent.dependencies = []  # TODO: add here list of module names that this module requires
-        self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # TODO: replace with "Firstname Lastname (Organization)"
-        # TODO: update with short description of the module and a link to online module documentation
-        # _() function marks text as translatable to other languages
+        self.parent.dependencies = []
+        self.parent.contributors = ["Santiramoli - ULPGC"]
         self.parent.helpText = _("""
-This is an example of scripted loadable module bundled in an extension.
-See more information in <a href="https://github.com/organization/projectname#SphereModule">module documentation</a>.
-""")
-        # TODO: replace with organization, grant and thanks
-        self.parent.acknowledgementText = _("""
-This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc., Andras Lasso, PerkLab,
-and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
-""")
-
-        # Additional initialization step after application startup is complete
-        slicer.app.connect("startupCompleted()", registerSampleData)
+                                    This is an example of a module for creating a volumetric sphere given two points.
+                                    More information: <a href="https://github.com/Santiramoli/SlicerTutorial.git"></a>.
+                                """
+                                )
 
 
-#
-# Register sample data sets in Sample Data module
-#
-
-
-def registerSampleData():
-    """Add data sets to Sample Data module."""
-    # It is always recommended to provide sample data for users to make it easy to try the module,
-    # but if no sample data is available then this method (and associated startupCompeted signal connection) can be removed.
-
-    import SampleData
-
-    iconsPath = os.path.join(os.path.dirname(__file__), "Resources/Icons")
-
-    # To ensure that the source code repository remains small (can be downloaded and installed quickly)
-    # it is recommended to store data sets that are larger than a few MB in a Github release.
-
-    # SphereModule1
-    SampleData.SampleDataLogic.registerCustomSampleDataSource(
-        # Category and sample name displayed in Sample Data module
-        category="SphereModule",
-        sampleName="SphereModule1",
-        # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
-        # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
-        thumbnailFileName=os.path.join(iconsPath, "SphereModule1.png"),
-        # Download URL and target file name
-        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
-        fileNames="SphereModule1.nrrd",
-        # Checksum to ensure file integrity. Can be computed by this command:
-        #  import hashlib; print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
-        checksums="SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
-        # This node name will be used when the data set is loaded
-        nodeNames="SphereModule1",
-    )
-
-    # SphereModule2
-    SampleData.SampleDataLogic.registerCustomSampleDataSource(
-        # Category and sample name displayed in Sample Data module
-        category="SphereModule",
-        sampleName="SphereModule2",
-        thumbnailFileName=os.path.join(iconsPath, "SphereModule2.png"),
-        # Download URL and target file name
-        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
-        fileNames="SphereModule2.nrrd",
-        checksums="SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
-        # This node name will be used when the data set is loaded
-        nodeNames="SphereModule2",
-    )
-
-
-#
-# SphereModuleParameterNode
-#
-
+# PARAMETERS FOR THE MODULE
 
 @parameterNodeWrapper
 class SphereModuleParameterNode:
@@ -294,7 +222,6 @@ class SphereModuleLogic(ScriptedLoadableModuleLogic):
         if not inputVolume or not outputVolume:
             raise ValueError("Input or output volume is invalid")
 
-        import time
 
         startTime = time.time()
         logging.info("Processing started")
@@ -350,8 +277,6 @@ class SphereModuleTest(ScriptedLoadableModuleTest):
         self.delayDisplay("Starting the test")
 
         # Get/create input data
-
-        import SampleData
 
         registerSampleData()
         inputVolume = SampleData.downloadSample("SphereModule1")
